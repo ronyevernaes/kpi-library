b8ab506906523e45b58ef19de11be137c75c17e2
Ronye Vernaes <remnahush@gmail.com>
2024-10-26 19:16:17 +0200

    add project types

---
+++ src/types.ts
@@ -0,0 +1,67 @@
+export enum AssetType {
+  KPI = 'KPI',
+  LAYOUT = 'LAYOUT',
+  DATA_VIZ = 'DATA_VIZ',
+  STORYBOARD = 'STORYBOARD',
+}
+
+export interface Asset {
+  id: string;
+  name: string;
+  description?: string;
+  type: AssetType;
+}
+
+export interface Kpi extends Asset {
+  targetValue: number;
+  currentValue: number;
+  targetDate: Date;
+  currentDate: Date;
+}
+
+export enum DataVizType {
+  BAR = 'BAR',
+  LINE = 'LINE',
+  PIE = 'PIE',
+  SCATTER = 'SCATTER',
+  HEATMAP = 'HEATMAP',
+}
+
+export interface DataViz extends Asset {
+  dataVizType: DataVizType;
+}
+
+export interface Layout extends Asset {
+  kpis: [Kpi, DataViz][];
+}
+
+export interface Storyboard extends Asset {
+  layout: Layout;
+  insights: string[];
+}
+
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  area: Area;
+  accessRequests: AccessRequest[];
+}
+
+export interface Area {
+  id: string;
+  name: string;
+  allowedKpiIds: string[];
+}
+
+export enum AccessRequestStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  DENIED = 'DENIED',
+}
+
+export interface AccessRequest {
+  user: User;
+  message: string;
+  asssets: Asset;
+}
