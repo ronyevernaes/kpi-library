9bb83ff2ae75af839aecbfc535f99fc6f2c998af
Ronye Vernaes <remnahush@gmail.com>
2024-10-27 17:45:04 +0100

    add title and text to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
+    <div className="flex flex-col gap-8">
+      <h1>Library</h1>
 
-    </main>
+      <p>Browse for assets needed to report and present analysis</p>
+    </div>
   );
 }
