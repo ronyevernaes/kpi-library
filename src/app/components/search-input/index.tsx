0dc8a637acac81d27572e5d76e1f13518ddd6e4c
Ronye Vernaes <remnahush@gmail.com>
2024-10-29 01:46:21 +0100

    refactor input search to make it work with query params and debounced behavior

--- src/app/components/search-input/index.tsx
+++ src/app/components/search-input/index.tsx
@@ -1,12 +1,31 @@
 'use client';
 
-import { FC } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import debounce from "debounce";
 
 import { InputText } from "@/src/components/ui";
-import { useStore } from "@/src/store";
 
 export const SearchInput: FC = () => {
-  const { search, setSearch } = useStore();
+  const [search, setSearch] = useState<string>("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const debouncedSearch = useMemo(() => debounce((searchString) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (searchString) {
+      params.set("search", searchString);
+    } else {
+      params.delete("search");
+    }
+
+    router.push(`/?${params.toString()}`);
+  }, 300), [router, searchParams]);
+
+  useEffect(() => {
+    debouncedSearch(search);
+  }, [debouncedSearch, search]);
 
   return (
     <InputText
